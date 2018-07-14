c7b7e8b87cb9ae9f3269f54c15a795bf5199a374
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-07-15 03:45:18 +0900

    debug内のexeファイルを指定するstring変数を作成および画面出力、実行はしてない

--- ColorVisionCorrection/ColorVisionCorrection/FileSearchCreate.cpp
+++ ColorVisionCorrection/ColorVisionCorrection/FileSearchCreate.cpp
@@ -13,11 +13,59 @@ reproduced or used in any manner whatsoever.
 
 
 
+void FileSearchCreate::SearchGameFile(std::string gamePath)
+{
+}
+
+void FileSearchCreate::SearchMediaFile(std::string gamePath)
+{
+}
+
 FileSearchCreate::FileSearchCreate()
 {
+	jpgFile = new int[50];
+	pngFile = new int[50];
+	bmpFile = new int[50];
+
+	GetModuleFileName(NULL, Path, MAX_PATH);	// 実行ファイルの完全パスを取得
+
+	_splitpath(Path, drive, dir, fname, ext);	// パス名を構成要素に分解する
+
+	exePath = "";
+
+	exePath.operator+=(drive);
+	exePath.operator+=(dir);
 }
 
 
 FileSearchCreate::~FileSearchCreate()
 {
+	delete[] jpgFile;
+	delete[] pngFile;
+	delete[] bmpFile;
+}
+
+void FileSearchCreate::Update()
+{
+	DrawFormatString(0, 0, 255, "%s", exePath.c_str());
+}
+
+std::vector<std::string> FileSearchCreate::GetImageFileName()
+{
+	return imageFileName;
+}
+
+int *FileSearchCreate::GetJPGFile()
+{
+	return jpgFile;
+}
+
+int *FileSearchCreate::GetPNGFile()
+{
+	return pngFile;
+}
+
+int *FileSearchCreate::GetBMPFile()
+{
+	return bmpFile;
 }
--- ColorVisionCorrection/ColorVisionCorrection/FileSearchCreate.hpp
+++ ColorVisionCorrection/ColorVisionCorrection/FileSearchCreate.hpp
@@ -10,22 +10,32 @@ reproduced or used in any manner whatsoever.
 */
 
 #pragma once
+#include "DxLib.h"
 #include <string>
 #include <vector>
 
 class FileSearchCreate
 {
 private:
-	//char Path[MAX_PATH + 1];	// 実行ファイルの完全パスを取得する変数
+	char Path[MAX_PATH + 1];	// 実行ファイルの完全パスを取得する変数
 
-	//char drive[MAX_PATH + 1]	// ドライブ
-	//	, dir[MAX_PATH + 1]		// ディレクトリパス
-	//	, fname[MAX_PATH + 1]	// ファイル名
-	//	, ext[MAX_PATH + 1];	// ファイル名の拡張子
+	char drive[MAX_PATH + 1]	// ドライブ
+		, dir[MAX_PATH + 1]		// ディレクトリパス
+		, fname[MAX_PATH + 1]	// ファイル名
+		, ext[MAX_PATH + 1];	// ファイル名の拡張子
 
+	std::vector<std::string> imageFileName;		// 画像のパスを拡張子までの情報を持つ者たち
+	int *jpgFile;								// JPG
+	int *pngFile;								// PNG
+	int *bmpFile;								// BMP
 	
+	std::string exePath;		// 本exeのパス
 
-	void SearchGameFile(std::string gamePath);
+	void SearchGameFile(std::string gamePath);													// exeと同じ階層に入れたフォルダ名を調べる
+	HANDLE hFind;																				// フォルダを確認する
+	int fileCount;																				// フォルダの数を得る
+	WIN32_FIND_DATA FindFileData;																// 確認したフォルダの名前を得る
+
 	void SearchMediaFile(std::string gamePath);
 
 public:
@@ -33,6 +43,12 @@ public:
 	~FileSearchCreate();
 
 	void Update();
+
+	// ゲッター
+	std::vector<std::string> GetImageFileName();
+	int *GetJPGFile();
+	int *GetPNGFile();
+	int *GetBMPFile();
 };
 
 /*
